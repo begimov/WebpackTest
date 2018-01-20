58bcc46d433be5f7a8e9f5c7a5bbcb0e17a117f1
begimov <begimov@gmail.com>
2018-01-20 18:24:14 +0300

    uglifyjs-webpack-plugin added to webpack production config

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,9 @@
 const base = require('./webpack.base.js')
 const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(base, {
-    //
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 })
