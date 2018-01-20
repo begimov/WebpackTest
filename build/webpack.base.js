4bb5a0360d2d1f56c2a7832f22b81f9bdae5bae8
begimov <begimov@gmail.com>
2018-01-20 17:55:53 +0300

    webpack.config moved to webpack.base file

---
+++ build/webpack.base.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'js/app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            // babelrc: false,
+                            // presets: ['@babel/preset-env'],
+                            // plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9090,
+        compress: true
+    }
+}
