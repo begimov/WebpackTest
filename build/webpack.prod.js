b40d1f2b953e42c8447d126c8af05cbc91acb270
begimov <begimov@gmail.com>
2018-01-22 18:36:02 +0300

    extracting styles to separate file implemented using extract-text-webpack-plugin for production builds

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,31 +2,29 @@ const base = require('./webpack.base.js')
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const env = require('../config/prod.env')
 
 module.exports = merge(base, {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
     plugins: [
         new UglifyJSPlugin(),
         new webpack.DefinePlugin({
             'process.env': JSON.stringify(env)
+        }),
+        new ExtractTextPlugin({
+            filename: '../dist/css/app.css'
         })
-    ],
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.scss$/,
-    //             use: [
-    //                 'style-loader',
-    //                 {
-    //                     loader: 'css-loader',
-    //                     options: {
-    //                         minimize: true
-    //                     }
-    //                 },
-    //                 'sass-loader'
-    //             ]
-    //         }
-    //     ]
-    // }
+    ]
 })
