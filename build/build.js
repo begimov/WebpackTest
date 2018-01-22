a12849f196c208ffc2381abb15cf8b7cdceaefc5
begimov <begimov@gmail.com>
2018-01-22 17:58:55 +0300

    using ora to generate spinner while building

--- build/build.js
+++ build/build.js
@@ -1,9 +1,15 @@
-const webpack = require('webpack')
-
 process.env.NODE_ENV = 'prod'
 
+const webpack = require('webpack')
 const config = require('./webpack.prod')
+const ora = require('ora')
+
+const spinner = ora('Building for production').start()
 
 webpack(config, function (err, stats) {
-    //
+    spinner.stop()
+
+    if (err) {
+        throw err
+    }
 })
