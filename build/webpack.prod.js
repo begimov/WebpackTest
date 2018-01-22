1254290b522418f010a8059c12972d4cfb26133e
begimov <begimov@gmail.com>
2018-01-22 17:21:50 +0300

    production webpack config updated to use enviroment config

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,11 +3,13 @@ const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack')
 
+const env = require('..config/prod.env')
+
 module.exports = merge(base, {
     plugins: [
         new UglifyJSPlugin(),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env': JSON.stringify(env)
         })
     ],
     // module: {
